a90d0e834d7c43baeaf6663f5173e8e49d36896c
Kurtis C Wright <kurtiscwright@gmail.com>
2022-06-15 00:25:30 -0700

    removed comment from sortPixels file and added unclaimed pixel to JSON file

--- scripts/sortPixels.js
+++ scripts/sortPixels.js
@@ -8,7 +8,6 @@ const writeFile = promisify(fs.writeFile);
 
 const pixelFilePath = path.join('_data', 'pixels.json');
 
-// Will read the current file, parse the contents, sort the pixels and write a sorted string of pixels to a file else it will write to console.
 readFile(pixelFilePath, { encoding: 'utf8' })
   .then(pixelFileData => {
     const pixels = JSON.parse(pixelFileData);
